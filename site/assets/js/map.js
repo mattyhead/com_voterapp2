cc14a3a35680c21d6767c087e45835c8f520d73b
Matthew Murphy <mattyhead@gmail.com>
2017-11-15 12:30:14 -0500

    ...

--- site/assets/js/map.js
+++ site/assets/js/map.js
@@ -771,7 +771,7 @@
         $.getJSON(service.url(input), service.params).done(function(response) {
             if (response.features) {
                 deferred.resolve({
-                    data: response.features[0]
+                    data: response.features[0].data
                 })
             } else {
                 deferred.reject()
